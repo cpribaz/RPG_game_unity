18dbdfee5190bfba272cdaa368c7d5189f8c79b6
cpribaz <cpribaz@uwo.ca>
2022-03-21 17:43:26 -0400

    added enemy AI

--- ProjectGame/Assets/_Scripts/Enemy.cs
+++ ProjectGame/Assets/_Scripts/Enemy.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    NavMeshAgent agent;
+
+    GameObject target;
+
+    private void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        target = GameObject.FindGameObjectWithTag("Player");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        findTarget();
+    }
+
+    private void findTarget()
+    {
+        agent.SetDestination(target.transform.position);
     }
 }
